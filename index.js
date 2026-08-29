9fe1d4c7186620de96f937e556bfc973c1495987
fabiokounang <fabiokounang11@gmail.com>
2026-08-29 21:01:00 +0700

    fixing allowed origin

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ app.use(
   helmet({
     contentSecurityPolicy: false,
     crossOriginEmbedderPolicy: false,
+    crossOriginResourcePolicy: { policy: 'cross-origin' }
   })
 );
 app.use(cors(corsOptions));
